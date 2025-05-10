e363ad1748416a9716503c77d49398f890baa4e2
Goosling <146563686+4ertilla228@users.noreply.github.com>
2025-05-10 15:29:07 +0500

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,9 @@
+ // Скрипт для часов в реальном времени
+    function updateClock() {
+        const now = new Date();
+        const hours = now.getHours().toString().padStart(2, '0');
+        const minutes = now.getMinutes().toString().padStart(2, '0');
+        document.getElementById('clock').textContent = `${hours}:${minutes}`;
+    }
+    setInterval(updateClock, 1000);
+    updateClock();
